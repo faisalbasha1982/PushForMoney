e02cad2f564cc7b4d6923c2f6d3a2b218fc4ffc5
Faisal <Development.dubai.5@jobfixers.be>
2018-09-05 12:34:52 +0000

    Deleted App

--- App/Api/api.js
+++
@@ -1,8 +0,0 @@
-const key = '!@M*;-kATy_vcUkLq/U))QD`XL5Sg`5D';
-
-const api = {
-  LoginUrl: `https://faazureapiinterfacedev.azurewebsites.net/api/fnMoblieLogin?code=${key}`,
-  AuthId: 'JS#236734',
-};
-
-export default api;
